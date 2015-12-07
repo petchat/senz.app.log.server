1e4526c474b4bd4b3eb3d54a9e7825c3c392d839
mageia <yzg96@gmail.com>
2015-12-07 12:58:20 +0800

    add login

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,19 +1,27 @@
 module.exports = function(server) {
     var router = server.loopback.Router();
-    //router.get('/', server.loopback.status());
 
     router.get('/', function (req, res) {
         res.render('index', { title: 'Hey', message: 'Hello there!'});
     });
 
-    router.use('/login', function(req, res, next){
-        res.render('login', { title: 'Hey', message: 'Hello there!'});
+    router.use('/login', function(req, res){
+        if(req.method == 'GET') res.render('login');
 
-        next();
+        if(req.method == 'POST'){
+            server.models.User.login(req.body).then(
+                function(d){
+                    res.send(d);
+                },
+                function(e){
+                    res.send(e);
+                }
+            );
+        }
     });
 
     router.get('/test', function(req, res){
-        res.render('test', { title: 'Hey', message: 'Hello there!'});
+        res.render('index', { title: 'Hey', message: 'Hello there!'});
     });
 
 
