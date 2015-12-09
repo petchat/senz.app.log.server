09e5ee99cbdccdd00f0730736c85d99b2acb9a4c
mageia <yzg96@gmail.com>
2015-12-09 17:09:06 +0800

    add middleware hook

--- server/boot/hook.js
+++ server/boot/hook.js
@@ -1,7 +1,6 @@
 module.exports = function(server) {
     var remotes = server.remotes();
     remotes.after('**', function (ctx, next) {
-        console.log(ctx);
         ctx.result = {id: ctx.result.id,
             createdAt: ctx.result.createdAt,
             userId: ctx.result.userId};
