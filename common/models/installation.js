9fc88df0fcfc929473ebc78fd8dda52492ee3b0c
mageia <yzg96@gmail.com>
2016-01-14 16:22:25 +0800

    many modify

--- common/models/installation.js
+++ common/models/installation.js
@@ -11,38 +11,69 @@ module.exports = function(Installation) {
 
         logger.debug("invent", hardwareId + " " + appId);
 
-        Promise.all([
-            Installation.app.models.senz_app.findOne({where:{"id": appId}}),
-            Installation.app.models.Tracker.findOne({where:{"hardwareId": hardwareId}})
-        ])
-        .then(
-            function (results) {
-                if(!results[0]) return Promise.reject("Invalid appId");
-
-                return results[1] || Installation.app.models.Tracker.create({
-                    "username": hardwareId + "_nologin",
-                    "hardwareId": hardwareId,
-                    "password": uuid.v4(),
-                    "email" : "nonexistence" + "@petchat.io"});
-            })
-        .then(
-            function (tracker) {
-                Installation.create({
-                    "userId": tracker.id,
-                    "appId": appId,
-                    "hardwareId": hardwareId,
-                    "deviceType": deviceType,
-                    "deviceToken": uuid.v4()
-                }, function(err, model){
-                    return cb(err, model);
-                })
-            })
-        .catch(
-            function(err){
-                logger.error(hardwareId, err);
-                return cb(err);
+        Installation.app.models.senz_app.findOne({where:{"id": appId}}, function(e, app){
+            if(e || !app){
+                return cb(e, "Invalid appId");
+            }else{
+                Installation.app.models.Tracker.findOne({where:{"username": hardwareId}},
+                    function(e, tracker){
+                        if(e || !tracker){
+                            Installation.app.models.Tracker.create({
+                                "username": hardwareId,
+                                "password": uuid.v4(),
+                                "email" : "nonexistence" + "@petchat.io"},
+                                function(e, tracker){
+                                    Installation.create({
+                                        "userId": tracker.id,
+                                        "appId": appId,
+                                        "hardwareId": hardwareId,
+                                        "deviceType": deviceType,
+                                        "deviceToken": uuid.v4()
+                                    }, function(err, installation){
+                                        logger.info(err, "Create New Installation: " + installation.id);
+                                        return cb(err, tracker);
+                                    })
+                                });
+                        }else{
+                            logger.info(e, "Use The Exist Tracker!");
+                            return cb(e, tracker);
+                        }
+                    })
             }
-        );
+        });
+
+        //Promise.all([
+        //    Installation.app.models.senz_app.findOne({where:{"id": appId}}),
+        //    Installation.app.models.Tracker.findOne({where:{"hardwareId": hardwareId}})
+        //])
+        //.then(
+        //    function (results) {
+        //        if(!results[0]) return Promise.reject("Invalid appId");
+        //
+        //        return results[1] || Installation.app.models.Tracker.create({
+        //            "username": hardwareId + "_nologin",
+        //            "hardwareId": hardwareId,
+        //            "password": uuid.v4(),
+        //            "email" : "nonexistence" + "@petchat.io"});
+        //    })
+        //.then(
+        //    function (tracker) {
+        //        Installation.create({
+        //            "userId": tracker.id,
+        //            "appId": appId,
+        //            "hardwareId": hardwareId,
+        //            "deviceType": deviceType,
+        //            "deviceToken": uuid.v4()
+        //        }, function(err, model){
+        //            return cb(err, model);
+        //        })
+        //    })
+        //.catch(
+        //    function(err){
+        //        logger.error(hardwareId, err);
+        //        return cb(err);
+        //    }
+        //);
     };
 
     Installation.remoteMethod(
